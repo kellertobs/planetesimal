c87b833016adfc3c2bd38ea0275340d95a93c56b
FakhriBintang <fakhri.bintang8@gmail.com>
2021-02-18 11:27:44 +0000

    fully vectorised temperature diffusion solver, could do with a bit more finesse

--- No air/src/Update_Temperature.m
+++ No air/src/Update_Temperature.m
@@ -37,58 +37,84 @@ end
 NP          = Nx*Nz; % total number of P nodes to solve + ghost nodes
 T_diff      = T_mid;
 ind         = reshape(1:NP,Nz,Nx);
-A           = sparse(Nz*Nx,Nz*Nx);
-RHS         = zeros(Nz*Nx,1);
 
 
+II  = [];
+JJ  = [];
+AA  = [];
+IR  = [];
+RR  = [];
 
 %% fill implicit matrices
-for i = 1:1:Nz
-for j = 1:1:Nx    
-    % setup boundary conditions
-    if (i == 1 || i == Nz || j==1 || j==Nx)
-        % Top BC, constant temperature
-        if (i==1 && j>1 && j<Nx)
-            A(ind(i,j),ind(i,j))    = 1;
-            A(ind(i,j),ind(i+1,j))  = 1;
-            RHS(ind(i,j))           = T_top*2;
-        end
-        % Bottom BC, constant temperature    
-        if (i==Nz && j>1 && j<Nx)
-            A(ind(i,j),ind(i,j))    = 1;
-            A(ind(i,j),ind(i-1,j))  = 1;
-            RHS(ind(i,j))           = T_mid(i,j)*2;
-        end
-        % Left BC, insulating boundary
-        if (j==1)
-            A(ind(i,j),ind(i,j))    = 1;
-            A(ind(i,j),ind(i,j+1))  = -1;
-            RHS(ind(i,j))           = 0;
-        end
-        % Right BC, Insulating boundary
-        if (j==Nx)
-            A(ind(i,j),ind(i,j))    = 1;
-            A(ind(i,j),ind(i,j-1))  = -1;
-            RHS(ind(i,j))           = 0;
-        end    
-    else   
-    % internal points
-    % fill A matrix
-    RhoCp_mid = Rho_mid(i,j)*Cp_mid(i,j);
-    A(ind(i,j),ind(i,j-1))      = -k_vx(i,j-1)/dx^2;     % left of current node
-    A(ind(i,j),ind(i,j+1))      = -k_vx(i,j)  /dx^2;     % right of current node
-    A(ind(i,j),ind(i-1,j))      = -k_vz(i-1,j)/dz^2;     % above current node
-    A(ind(i,j),ind(i+1,j))      = -k_vz(i,j)  /dz^2;     % below current node
-    A(ind(i,j),ind(i,j))        = (RhoCp_mid/dt) + ...
-                                ((k_vx(i,j) + k_vx(i,j-1))/dx^2) + ...
-                                ((k_vz(i,j) + k_vz(i-1,j))/dz^2); %current node
-                            
-    % fill RHS vector
-    RHS(ind(i,j))               = (Hs(i,j) + Ha(i,j) + Hr(i,j)) +...
-                                   RhoCp_mid*T_mid(i,j)/dt;
-    end
-end
-end
+
+
+
+% internal points
+%auxilary array
+ii = ind(2:nz1,2:nx1); % current node
+%left of current
+jj = ind(2:nz1,1:nx1-1); kk = ind(2:nz1,1:nx1-1);
+II = [II, ii(:)'];   JJ = [JJ, jj(:)'];   AA = [AA, -k_vx(kk(:)')/dx^2];
+%right of current
+jj = ind(2:nz1,3:nx1+1); kk = ind(2:nz1,2:nx1);
+II = [II, ii(:)'];   JJ = [JJ, jj(:)'];   AA = [AA, -k_vx(kk(:)')/dx^2];
+%above current
+jj = ind(1:nz1-1,2:nx1); kk = ind(1:nz1-1,2:nx1);
+II = [II, ii(:)'];   JJ = [JJ, jj(:)'];   AA = [AA, -k_vz(kk(:)')/dz^2];
+%below current
+jj = ind(3:nz1+1,2:nx1); kk = ind(2:nz1,2:nx1);
+II = [II, ii(:)'];   JJ = [JJ, jj(:)'];   AA = [AA, -k_vz(kk(:)')/dz^2];
+%current node
+RhoCp_mid = Rho_mid(2:nz1,2:nx1).*Cp_mid(2:nz1,2:nx1);
+Asum = (RhoCp_mid./dt)...
+    +((k_vx(2:nz1,2:nx1) + k_vx(2:nz1,1:nx1-1))/dx^2)...
+    +((k_vz(2:nz1,2:nx1) + k_vz(1:nz1-1,2:nx1))/dz^2);
+jj = ii;
+II = [II, ii(:)'];   JJ = [JJ, jj(:)'];   AA = [AA, Asum(:)'];
+%RHS
+RRSUM = (Hs(2:nz1,2:nx1)+Ha(2:nz1,2:nx1)+Hr(2:nz1,2:nx1))+RhoCp_mid.*T_mid(2:nz1,2:nx1)./dt;
+IR = [IR, ii(:)']; RR = [RR, RRSUM(:)'];
+
+% boundary points
+%top constant temp
+ii  = ind(1,2:nx1); jj  = ind(1,2:nx1); jj1 = ind(2,2:nx1); %below top boundary
+Asum = zeros(size(ii));
+II = [II, ii(:)']; JJ = [JJ, jj(:)'];   AA = [AA, Asum(:)'+1];
+II = [II, ii(:)']; JJ = [JJ, jj1(:)'];  AA = [AA, Asum(:)'+1];
+%RHS
+IR = [IR, ii(:)']; RR = [RR, Asum(:)'*T_top*2];
+    
+%bottom constant temp
+ii  = ind(Nz,2:nx1); jj  = ind(Nz,2:nx1); jj1 = ind(nz1,2:nx1); %below top boundary
+Asum = zeros(size(ii)); Asum1 = Asum.*T_mid(Nz,2:nx1).*2;
+II = [II, ii(:)']; JJ = [JJ, jj(:)'];   AA = [AA, Asum(:)'+1];
+II = [II, ii(:)']; JJ = [JJ, jj1(:)'];  AA = [AA, Asum(:)'+1];
+%RHS
+IR = [IR, ii(:)']; RR = [RR, Asum1(:)'];
+
+%left insulating boundary
+ii  = ind(1:Nz,1); jj  = ind(1:Nz,1); jj1 = ind(1:Nz,2); %below top boundary
+Asum = zeros(size(ii));
+II = [II, ii(:)']; JJ = [JJ, jj(:)'];   AA = [AA, Asum(:)'+1];
+II = [II, ii(:)']; JJ = [JJ, jj1(:)'];  AA = [AA, Asum(:)'-1];
+%RHS
+IR = [IR, ii(:)']; RR = [RR, Asum(:)'];
+
+%right insulating boundary
+ii  = ind(1:Nz,Nx); jj  = ind(1:Nz,Nx); jj1 = ind(1:Nz,nx1); %below top boundary
+Asum = zeros(size(ii));
+II = [II, ii(:)']; JJ = [JJ, jj(:)'];   AA = [AA, Asum(:)'+1];
+II = [II, ii(:)']; JJ = [JJ, jj1(:)'];  AA = [AA, Asum(:)'-1];
+%RHS
+IR = [IR, ii(:)']; RR = [RR, Asum(:)'];
+
+%         if (j==1)
+%             II = [II, ind(i,j)]; JJ = [JJ, ind(i,j)];   AA = [AA, 1];
+%             II = [II, ind(i,j)]; JJ = [JJ, ind(i,j+1)]; AA = [AA, -1];
+%             IR = [IR, ind(i,j)]; RR = [RR, 0];
+
+A       = sparse(II,JJ,AA,NP,NP);
+RHS     = sparse(IR,ones(size(IR)),RR,NP,1);
 
 X           =  sqrt(abs(diag(A)));
 X           =  diag(sparse(1./X));
@@ -110,12 +136,63 @@ dT          = T_diff-T_mid;
 
 % thermal timestepping conditions
 maxdT       = max(max(abs(dT)));
+
+% legacy loop for A matrix filler
 % if titer<2 && maxdT>dTmax
 %     dt      = dt/maxdT*dTmax;
 % else
 %     break
 % end
-
+% for i = 1:1:Nz
+% for j = 1:1:Nx    
+%     % setup boundary conditions
+%     if (i == 1 || i == Nz || j==1 || j==Nx)
+%         % Top BC, constant temperature
+%         if (i==1 && j>1 && j<Nx)
+%             II = [II, ind(i,j)]; JJ = [JJ, ind(i,j)];   AA = [AA, 1];
+%             II = [II, ind(i,j)]; JJ = [JJ, ind(i+1,j)]; AA = [AA, 1];
+%             IR = [IR, ind(i,j)]; RR = [RR, T_top*2];
+%         end
+%         % Bottom BC, constant temperature    
+%         if (i==Nz && j>1 && j<Nx)
+%             II = [II, ind(i,j)]; JJ = [JJ, ind(i,j)];   AA = [AA, 1];
+%             II = [II, ind(i,j)]; JJ = [JJ, ind(i-1,j)]; AA = [AA, 1];
+%             IR = [IR, ind(i,j)]; RR = [RR, T_mid(i,j)*2];
+%             
+%         end
+%         % Left BC, insulating boundary
+%         if (j==1)
+%             II = [II, ind(i,j)]; JJ = [JJ, ind(i,j)];   AA = [AA, 1];
+%             II = [II, ind(i,j)]; JJ = [JJ, ind(i,j+1)]; AA = [AA, -1];
+%             IR = [IR, ind(i,j)]; RR = [RR, 0];
+%             
+%         end
+%         % Right BC, Insulating boundary
+%         if (j==Nx)
+%             II = [II, ind(i,j)]; JJ = [JJ, ind(i,j)];   AA = [AA, 1];
+%             II = [II, ind(i,j)]; JJ = [JJ, ind(i,j-1)]; AA = [AA, -1];
+%             IR = [IR, ind(i,j)]; RR = [RR, 0];
+%             
+%         end    
+%     else   
+%     % internal points
+%     % fill A matrix
+%     RhoCp_mid = Rho_mid(i,j)*Cp_mid(i,j);
+%     Asum = (RhoCp_mid/dt)+((k_vx(i,j) + k_vx(i,j-1))/dx^2)+((k_vz(i,j) + k_vz(i-1,j))/dz^2);
+%     
+% %     II = [II, ind(i,j)]; JJ = [JJ, ind(i,j-1)];   AA = [AA, -k_vx(i,j-1)/dx^2]; % left of current node
+% %     II = [II, ind(i,j)]; JJ = [JJ, ind(i,j+1)];   AA = [AA, -k_vx(i,j)  /dx^2]; % right of current node
+% %     II = [II, ind(i,j)]; JJ = [JJ, ind(i-1,j)];   AA = [AA, -k_vz(i-1,j)/dz^2]; % above current node
+% %     II = [II, ind(i,j)]; JJ = [JJ, ind(i+1,j)];   AA = [AA, -k_vz(i,j)  /dz^2]; % below current node
+%     II = [II, ind(i,j)]; JJ = [JJ, ind(i,j)];     AA = [AA, Asum];
+% 
+%                                                               % current node
+%     %RHS
+%     IR = [IR, ind(i,j)]; RR = [RR, (Hs(i,j)+Ha(i,j)+Hr(i,j))+RhoCp_mid*T_mid(i,j)/dt];
+%                             
+%     end
+% end
+% end
 
 
             
