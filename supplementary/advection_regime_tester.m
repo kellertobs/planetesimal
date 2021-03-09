333769c146edcbbf1dd3409ca1a8231d71c681a2
FakhriBintang <fakhri.bintang8@gmail.com>
2021-03-09 15:49:43 +0000

    updated advection regime tester

--- supplementary/advection_regime_tester.m
+++ supplementary/advection_regime_tester.m
@@ -1,20 +1,22 @@
-% test and compare advection schemes
-
 % prepare workspace
 clear all
-close all
-
+% close all
+% profile off
+% profile on
 % set run options
-RunID       = 'gaussian_rotation';
+% RunID       = 'gaussian_rotation';
 movement    = 'rotation';
 L           = 1;     % domain length
-N           = 200;   % number of grid points
+N           = 50;   % number of grid points
 h           = L/N;   % grid spacing
-CFL         = 0.25;  % Courant number
-M           = 100;   % number of time steps to take
+CFL         = 0.1;  % Courant number
+theta       = 1;  % time stepping mode (0.5 = Crank-Nicolson, 0 = backward Euler, 1 = forward Euler)
+M           = 3;   % number of time steps to take
 a0          = 1;     % initialbackground value of advected quantity
 a1          = 1;     % initial amplitude of advected quantity
 radius      = L/20;  % initial radius of gaussian in advected quantity
+
+% RunID = ['CFL' num2str(CFL) ' dx' num2str(h)]
 
 % get coordinate grids
 % interior cell nodes
@@ -44,13 +46,15 @@ w   = zeros(N+1,N+2);  % staggered vz
 % set initial condition for advected quantity
 x0  = L/6; z0 = L/6;
 gsn = exp(- (xa2d-x0).^2./radius.^2 - (za2d-z0).^2./radius.^2 );
-a   = a0 + a1.*gsn; 
+a   = a0 + a1.*gsn;
+
+% if ~exist([pwd '/out/',RunID],'dir'); mkdir([pwd '/out/',RunID]); end
 
 % set velocity field
 switch movement
     case 'rotation'
         u = -zu2d;
-        w =  xw2d;  
+        w =  xw2d;
         umid = -za2d;
         wmid =  xa2d;
 end
@@ -66,10 +70,10 @@ fprintf(1,'\n\n*****  Advection Scheme Tester: N = %d;  CFL = %1.3f;  M = %d;\n\
 
 time = 0;
 dt   = CFL * min( h/2/max(abs(u(:))) , h/2/max(abs(w(:))) );
-ti   = 0;
+ti   = 1;
 
-while max([max(afr(:))-min(afr(:)),max(au2(:))-min(au2(:)),max(au3(:))-min(au3(:))]) < 100*a1 && ti <= M
-
+while max([max(afr(:))-min(afr(:)),max(au3(:))-min(au3(:))]) < 1.5*a1 && ti <= M
+% while time<=time_end
     % store previous advection rates
     dadtcdo = dadtcd;
     dadtfdo = dadtfd;
@@ -77,7 +81,7 @@ while max([max(afr(:))-min(afr(:)),max(au2(:))-min(au2(:)),max(au3(:))-min(au3(:
     dadtu1o = dadtu1;
     dadtu2o = dadtu2;
     dadtu3o = dadtu3;
-
+    
     dadtcd  = advection(u,w,acd,h,a0,'centred FD');
     dadtfd  = advection(u,w,afd,h,a0,'flxdiv');
     dadtfr  = advection(u,w,afr,h,a0,'fromm');
@@ -86,25 +90,25 @@ while max([max(afr(:))-min(afr(:)),max(au2(:))-min(au2(:)),max(au3(:))-min(au3(:
     dadtu3  = advection(u,w,au3,h,a0,'third upwind');
     
     % advance advection time step
-    acd(2:end-1,2:end-1)    = acd(2:end-1,2:end-1)  - (dadtcd+dadtcdo)/2*dt;
-    afd(2:end-1,2:end-1)    = afd(2:end-1,2:end-1)  - (dadtfd+dadtfdo)/2*dt;
-    afr(2:end-1,2:end-1)    = afr(2:end-1,2:end-1)  - (dadtfr+dadtfro)/2*dt;
-    au1(2:end-1,2:end-1)    = au1(2:end-1,2:end-1)  - (dadtu1+dadtu1o)/2*dt;
-    au2(2:end-1,2:end-1)    = au2(2:end-1,2:end-1)  - (dadtu2+dadtu2o)/2*dt;
-    au3(2:end-1,2:end-1)    = au3(2:end-1,2:end-1)  - (dadtu3+dadtu3o)/2*dt;
-
+    acd(2:end-1,2:end-1)    = acd(2:end-1,2:end-1)  - (theta.*dadtcd+(1-theta).*dadtcdo)*dt;
+    afd(2:end-1,2:end-1)    = afd(2:end-1,2:end-1)  - (theta.*dadtfd+(1-theta).*dadtfdo)*dt;
+    afr(2:end-1,2:end-1)    = afr(2:end-1,2:end-1)  - (theta.*dadtfr+(1-theta).*dadtfro)*dt;
+    au1(2:end-1,2:end-1)    = au1(2:end-1,2:end-1)  - (theta.*dadtu1+(1-theta).*dadtu1o)*dt;
+    au2(2:end-1,2:end-1)    = au2(2:end-1,2:end-1)  - (theta.*dadtu2+(1-theta).*dadtu2o)*dt;
+    au3(2:end-1,2:end-1)    = au3(2:end-1,2:end-1)  - (theta.*dadtu3+(1-theta).*dadtu3o)*dt;
+    
     % advance reference gaussian
-    x0   = x0 + interp2(xa2d,za2d,umid,x0,z0)*dt; 
+    x0   = x0 + interp2(xa2d,za2d,umid,x0,z0)*dt;
     z0 = z0 + interp2(xa2d,za2d,wmid,x0,z0)*dt;
     gsn  = exp(- (xa2d-x0).^2./radius.^2 - (za2d-z0).^2./radius.^2 );
     
     res_acd = acd - (a0 + a1.*gsn);
     res_afd = afd - (a0 + a1.*gsn);
-    res_afr = afr - (a0 + a1.*gsn); 
-    res_au1 = au1 - (a0 + a1.*gsn); 
-    res_au2 = au2 - (a0 + a1.*gsn); 
-    res_au3 = au3 - (a0 + a1.*gsn); 
-
+    res_afr = afr - (a0 + a1.*gsn);
+    res_au1 = au1 - (a0 + a1.*gsn);
+    res_au2 = au2 - (a0 + a1.*gsn);
+    res_au3 = au3 - (a0 + a1.*gsn);
+    
     resnorm_acd = norm(res_acd(:),2)./norm(a0+a1.*gsn,2);
     resnorm_afd = norm(res_afd(:),2)./norm(a0+a1.*gsn,2);
     resnorm_afr = norm(res_afr(:),2)./norm(a0+a1.*gsn,2);
@@ -112,93 +116,162 @@ while max([max(afr(:))-min(afr(:)),max(au2(:))-min(au2(:)),max(au3(:))-min(au3(:
     resnorm_au2 = norm(res_au2(:),2)./norm(a0+a1.*gsn,2);
     resnorm_au3 = norm(res_au3(:),2)./norm(a0+a1.*gsn,2);
     
-    fprintf(1,'   --- %d; cntrfd %1.3e; flxdiv %1.3e; fromm %1.3e; upw1 %1.3e; upw2 %1.3e; upw3 %1.3e;\n',ti,resnorm_acd,resnorm_afd,resnorm_afr,resnorm_au1,resnorm_au2,resnorm_au3);
-        
-    % plot results
-    if ~mod(ti,10)  
-        
-        figure(1)
-        subplot(2,3,1)
-        imagesc(x,z,acd); axis equal tight; colorbar
-        title('FD advection')
-        
-        subplot(2,3,2)
-        imagesc(x,z,afd); axis equal tight; colorbar
-        title('FD flux divergence')
-        
-        subplot(2,3,3)
-        imagesc(x,z,afr); axis equal tight; colorbar
-        title('Fromm scheme')
-        
-        subplot(2,3,4)
-        imagesc(x,z,au1); axis equal tight; colorbar
-        title('1st order upwind')
-        
-        subplot(2,3,5)
-        imagesc(x,z,au2); axis equal tight; colorbar
-        title('2nd order upwind')
-        
-        subplot(2,3,6)
-        imagesc(x,z,au3); axis equal tight; colorbar
-        title('3rd order upwind')
-        drawnow;
-        
-        figure(2)
-        subplot(2,3,1)
-        imagesc(x,z,res_acd./norm(a0+a1.*gsn,2)); axis equal tight; colorbar
-        title('FD advection')
-        
-        subplot(2,3,2)
-        imagesc(x,z,res_afd./norm(a0+a1.*gsn,2)); axis equal tight; colorbar
-        title('FD flux divergence')
-        
-        subplot(2,3,3)
-        imagesc(x,z,res_afr./norm(a0+a1.*gsn,2)); axis equal tight; colorbar
-        title('Fromm scheme')
-        
-        subplot(2,3,4)
-        imagesc(x,z,res_au1./norm(a0+a1.*gsn,2)); axis equal tight; colorbar
-        title('1st order upwind')
-        
-        subplot(2,3,5)
-        imagesc(x,z,res_au2./norm(a0+a1.*gsn,2)); axis equal tight; colorbar
-        title('2nd order upwind')
-        
-        subplot(2,3,6)
-        imagesc(x,z,res_au3./norm(a0+a1.*gsn,2)); axis equal tight; colorbar
-        title('3rd order upwind')
-        drawnow;
-        
-%     figure(2)
-%     subplot(2,2,1)
-%     quiver(x(1:10:end),z(1:10:end),umid(2:10:end-1,1:10:end),wmid(1:10:end,2:10:end-1))
-%     axis ij image
-%     
-%     subplot(2,2,2)
-%     imagesc(x,z,dadtf(2:end-1,2:end-1))
-%     colorbar
-%     title('fromm scheme da/dt')
-%     
-%     subplot(2,2,3)
-%     imagesc(x,z,dadtu3(2:end-1,2:end-1))
-%     colorbar
-%     title('3rd order upwind da/dt')
-%     
-%     subplot(2,2,4)
-%     imagesc(x,z,dadtu2(2:end-1,2:end-1))
-%     colorbar
-%     title('2nd order upwind da/dt')
+    %     fprintf(1,'   --- %d; cntrfd %1.3e; flxdiv %1.3e; fromm %1.3e; upw1 %1.3e; upw2 %1.3e; upw3 %1.3e;\n',ti,resnorm_acd,resnorm_afd,resnorm_afr,resnorm_au1,resnorm_au2,resnorm_au3);
+    %     figure(3)
+    %     loglog(ti,resnorm_acd,'*r')
+    %     hold on
+    %     loglog(ti,resnorm_afd,'*b')
+    %     hold on
+    %     loglog(ti,resnorm_afr,'*k')
+    %     hold on
+    %     loglog(ti,resnorm_au1,'*c')
+    %     hold on
+    %     loglog(ti,resnorm_au2,'*g')
+    %     hold on
+    %     loglog(ti,resnorm_au3,'*y')
+    %     hold on
+    %     legend('Central difference','flux divergent','fromm','first upwind','second upwind','third upwind')
+    %     xlabel(['iterations']);
+    %     ylabel('Residuals');
     
-%     pause(0.1)
-%     saveas(figure(1),[pwd '/out/', RunID, ' ', num2str(ti), 'iterations.jpg'])
-%     saveas(figure(2),[pwd '/out/dadt ', RunID, ' ', num2str(ti), 'iterations.jpg'])
-    end
+    % plot results
+    %     if ~mod(ti,10)
+    %
+    %
+    %         figure(1)
+    %         subplot(2,3,1)
+    %         imagesc(x,z,acd); axis equal tight; colorbar
+    %         title('FD advection')
+    %
+    %         subplot(2,3,2)
+    %         imagesc(x,z,afd); axis equal tight; colorbar
+    %         title('FD flux divergence')
+    %
+    %         subplot(2,3,3)
+    %         imagesc(x,z,afr); axis equal tight; colorbar
+    %         title('Fromm scheme')
+    %
+    %         subplot(2,3,4)
+    %         imagesc(x,z,au1); axis equal tight; colorbar
+    %         title('1st order upwind')
+    %
+    %         subplot(2,3,5)
+    %         imagesc(x,z,au2); axis equal tight; colorbar
+    %         title('2nd order upwind')
+    %
+    %         subplot(2,3,6)
+    %         imagesc(x,z,au3); axis equal tight; colorbar
+    %         title('3rd order upwind')
+    %         drawnow;
+    %
+    %         figure(2)
+    %         subplot(2,3,1)
+    %         imagesc(x,z,res_acd./norm(a0+a1.*gsn,2)); axis equal tight; colorbar
+    %         title('FD advection')
+    %
+    %         subplot(2,3,2)
+    %         imagesc(x,z,res_afd./norm(a0+a1.*gsn,2)); axis equal tight; colorbar
+    %         title('FD flux divergence')
+    %
+    %         subplot(2,3,3)
+    %         imagesc(x,z,res_afr./norm(a0+a1.*gsn,2)); axis equal tight; colorbar
+    %         title('Fromm scheme')
+    %
+    %         subplot(2,3,4)
+    %         imagesc(x,z,res_au1./norm(a0+a1.*gsn,2)); axis equal tight; colorbar
+    %         title('1st order upwind')
+    %
+    %         subplot(2,3,5)
+    %         imagesc(x,z,res_au2./norm(a0+a1.*gsn,2)); axis equal tight; colorbar
+    %         title('2nd order upwind')
+    %
+    %         subplot(2,3,6)
+    %         imagesc(x,z,res_au3./norm(a0+a1.*gsn,2)); axis equal tight; colorbar
+    %         title('3rd order upwind')
+    %         drawnow;
+    
+    %         saveas(figure(1),[pwd '/out/', RunID, '/', num2str(ti), 'iterations.jpg'])
+    %         saveas(figure(2),[pwd '/out/', RunID, '/dadt ', num2str(ti), 'iterations.jpg'])
+    
+    %     figure(2)
+    %     subplot(2,2,1)
+    %     quiver(x(1:10:end),z(1:10:end),umid(2:10:end-1,1:10:end),wmid(1:10:end,2:10:end-1))
+    %     axis ij image
+    %
+    %     subplot(2,2,2)
+    %     imagesc(x,z,dadtf(2:end-1,2:end-1))
+    %     colorbar
+    %     title('fromm scheme da/dt')
+    %
+    %     subplot(2,2,3)
+    %     imagesc(x,z,dadtu3(2:end-1,2:end-1))
+    %     colorbar
+    %     title('3rd order upwind da/dt')
+    %
+    %     subplot(2,2,4)
+    %     imagesc(x,z,dadtu2(2:end-1,2:end-1))
+    %     colorbar
+    %     title('2nd order upwind da/dt')
+    
+    %     pause(0.1)
+    %     saveas(figure(1),[pwd '/out/', RunID, ' ', num2str(ti), 'iterations.jpg'])
+    %     saveas(figure(2),[pwd '/out/dadt ', RunID, ' ', num2str(ti), 'iterations.jpg'])
+    %     end
     
     % update time step
     ti = ti+1;
     time = time +dt;
     
 end
+fprintf(1,'   --- %d; cntrfd %1.3e; flxdiv %1.3e; fromm %1.3e; upw1 %1.3e; upw2 %1.3e; upw3 %1.3e;\n',ti,resnorm_acd,resnorm_afd,resnorm_afr,resnorm_au1,resnorm_au2,resnorm_au3);
+% figure()
+% subplot(2,3,1)
+% imagesc(x,z,acd); axis equal tight; colorbar
+% title('FD advection')
+%
+% subplot(2,3,2)
+% imagesc(x,z,afd); axis equal tight; colorbar
+% title('FD flux divergence')
+%
+% subplot(2,3,3)
+% imagesc(x,z,afr); axis equal tight; colorbar
+% title('Fromm scheme')
+%
+% subplot(2,3,4)
+% imagesc(x,z,au1); axis equal tight; colorbar
+% title('1st order upwind')
+%
+% subplot(2,3,5)
+% imagesc(x,z,au2); axis equal tight; colorbar
+% title('2nd order upwind')
+%
+% subplot(2,3,6)
+% imagesc(x,z,au3); axis equal tight; colorbar
+% title('3rd order upwind')
+% drawnow;
+%
+% figure(2)
+% plot(x0,z0,'*')
+% hold on
+% xlim([x(1) x(end)])
+% ylim([z(1) z(end)])
+
+
+figure(4)
+loglog(h,resnorm_acd,'-*r')
+hold on
+loglog(h,resnorm_afd,'-*b')
+hold on
+loglog(h,resnorm_afr,'-*k')
+hold on
+loglog(h,resnorm_au1,'-*c')
+hold on
+loglog(h,resnorm_au2,'-*g')
+hold on
+loglog(h,resnorm_au3,'-*y')
+hold on
+legend('Central difference','flux divergent','fromm','first upwind','second upwind','third upwind')
+xlabel('dx'); ylabel('Residuals');
 
 function adv = advection(u,w,a,h,a0,output)
 
@@ -212,7 +285,7 @@ vz  = (wp+wm)./2;
 
 vxp = max(vx,0); vxm = min(vx,0);
 vzp = max(vz,0); vzm = min(vz,0);
-        
+
 agh                    = zeros(size(a)+2);
 agh(2:end-1,2:end-1)   = a;
 
@@ -226,7 +299,7 @@ aip = agh(3:end-2,4:end-1);  aipp = agh(3:end-2,5:end-0);
 aim = agh(3:end-2,2:end-3);  aimm = agh(3:end-2,1:end-4);
 
 switch output
-
+    
     case 'centred FD'
         
         adv = vx.*(aip-aim)./h + vz.*(ajp-ajm)./h;
@@ -236,19 +309,19 @@ switch output
         adv = ((ajp+acc)./2.*wp - (ajm+acc)./2.*wm)./h ...
             + ((aip+acc)./2.*up - (aim+acc)./2.*um)./h ...
             - acc.*(diff(w(:,2:end-1),1,1)./h + diff(u(2:end-1,:),1,2)./h);
-   
+        
     case 'fromm'
-
+        
         adv   =     up .*(-(aipp-aip)./h./8 + (aip + acc)./h./2 + (acc-aim )./h./8) ...
-              - abs(up).*(-(aipp-aip)./h./8 + (aip - acc)./h./4 - (acc-aim )./h./8) ...
-              -     um .*(-(aip -acc)./h./8 + (acc + aim)./h./2 + (aim-aimm)./h./8) ...
-              + abs(um).*(-(aip -acc)./h./8 + (acc - aim)./h./4 - (aim-aimm)./h./8) ...
-              +     wp .*(-(ajpp-ajp)./h./8 + (ajp + acc)./h./2 + (acc-ajm )./h./8) ...
-              - abs(wp).*(-(ajpp-ajp)./h./8 + (ajp - acc)./h./4 - (acc-ajm )./h./8) ...
-              -     wm .*(-(ajp -acc)./h./8 + (acc + ajm)./h./2 + (ajm-ajmm)./h./8) ...
-              + abs(wm).*(-(ajp -acc)./h./8 + (acc - ajm)./h./4 - (ajm-ajmm)./h./8) ...
-              - acc.*(diff(w(:,2:end-1),1,1)./h + diff(u(2:end-1,:),1,2)./h);
-          
+            - abs(up).*(-(aipp-aip)./h./8 + (aip - acc)./h./4 - (acc-aim )./h./8) ...
+            -     um .*(-(aip -acc)./h./8 + (acc + aim)./h./2 + (aim-aimm)./h./8) ...
+            + abs(um).*(-(aip -acc)./h./8 + (acc - aim)./h./4 - (aim-aimm)./h./8) ...
+            +     wp .*(-(ajpp-ajp)./h./8 + (ajp + acc)./h./2 + (acc-ajm )./h./8) ...
+            - abs(wp).*(-(ajpp-ajp)./h./8 + (ajp - acc)./h./4 - (acc-ajm )./h./8) ...
+            -     wm .*(-(ajp -acc)./h./8 + (acc + ajm)./h./2 + (ajm-ajmm)./h./8) ...
+            + abs(wm).*(-(ajp -acc)./h./8 + (acc - ajm)./h./4 - (ajm-ajmm)./h./8) ...
+            - acc.*(diff(w(:,2:end-1),1,1)./h + diff(u(2:end-1,:),1,2)./h);
+        
     case 'first upwind'
         
         axp   = (aip-acc)./h;
@@ -262,7 +335,7 @@ switch output
         adv   = daxdt + dazdt;
         
     case 'second upwind'
-
+        
         axp   = (-3*acc+4*aip-aipp)/2/h;
         axm   = ( 3*acc-4*aim+aimm)/2/h;
         azp   = (-3*acc+4*ajp-ajpp)/2/h;
@@ -271,10 +344,10 @@ switch output
         daxdt = vxp.*axm + vxm.*axp;
         dazdt = vzp.*azm + vzm.*azp;
         
-        adv   = daxdt + dazdt;        
+        adv   = daxdt + dazdt;
         
     case 'third upwind'
-
+        
         axp   = (-2*aim-3*acc+6*aip-aipp)/6/h;
         axm   = ( 2*aip+3*acc-6*aim+aimm)/6/h;
         azp   = (-2*ajm-3*acc+6*ajp-ajpp)/6/h;
@@ -284,6 +357,6 @@ switch output
         dazdt = vzp.*azm + vzm.*azp;
         
         adv   = daxdt + dazdt;
-            
-end  
+        
+end
 end
--- upwind advection/src/main_loop.m
+++ upwind advection/src/main_loop.m
@@ -1,16 +1,8 @@
 %main model loop, including functions below
 time            = 0;     % initialise time loop
-
+lapl_T0 = 0; adv_T0 = 0; dMdt0 = 0;
 %% main loop
 for ti = 1:nt
-    profile on
-    % % =============================================================
-    % % update grid
-    % % =============================================================
-    % interpolate markes onto eulerian grid
-%     [Eta_out,Eta_mid,k_vz,k_vx,Cp_mid,T_mid,Alpha_mid,Hr,Material,Alpha_vx,Alpha_vz,T_vx,T_vz]...
-%     = Marker2grid(marknum,nx,nx1,Nx,nz,nz1,Nz,dx,dz,xm,zm,...
-%     x,z,xvx,zvx,xvz,zvz,xp,zp,Etam,Kappam,Tm,Cpm,Alpham,Hrm,Mtype);
 
     Alpha_vx(:,2:nx1) = (Alpha_mid(:,2:nx1)+Alpha_mid(:,1:nx))./2;
     Alpha_vz(2:nz1,:) = (Alpha_mid(2:nz1,:)+Alpha_mid(1:nz,:))./2;
@@ -23,17 +15,17 @@ for ti = 1:nt
     
     % Apply thermal boundary condition to interpolated nodes
     % upper boundary, constant temperature 
-    T_mid(2,2:nx)   = 2*T_top   - T_mid(3,2:nx);
-    T_vx(1,2:nx)    = 2*T_top   - T_vx(2,2:nx);
-    T_vz(1,2:nx1)   = 2*T_top   - T_vz(2,2:nx1);
-    % lower boundary, constant temperature
-    T_mid(nz1,2:nx1)= 2*(T_bot)   - T_mid(nz,2:nx1);
-    T_vx(Nz,2:nx)   = 2*(T_bot)   - T_vx(nz1,2:nx);
-    T_vz(nz1,2:nx1) = 2*(T_bot)   - T_vz(nz,2:nx1);    
+    T_mid(1,:)   = 2*T_top   - T_mid(3,:);
+    T_vx(1,:)    = 2*T_top   - T_vx(2,:);
+    T_vz(1,:)   = 2*T_top   - T_vz(2,:);
     
-%     T_mid(nz1,2:nx1)= 2*(T_bot+300)   - T_mid(nz,2:nx1);
-%     T_vx(nz1:Nz,2:nx)   = 2*(T_bot+300)   - T_vx(nz1,2:nx);
-%     T_vz(nz1:Nz,2:nx1) = 2*(T_bot+300)   - T_vz(nz,2:nx1);  
+    % lower boundary, constant temperature
+    T_mid(end,:)= 2*(T_bot)   - T_mid(nz,:);   
+    T_vx(end,:)   = 2*(T_bot)   - T_vx(nz1,:);
+    T_vz(end,:) = 2*(T_bot)   - T_vz(nz,:);    
+%       T_mid(end,:) = T_bot;
+%       T_vx(end,:) = T_bot;
+%       T_vz(end,:) = T_bot;
     
     % left boundary, insulating boundary
     T_mid(2:nz1,2)  = T_mid(2:nz1,3);
@@ -44,105 +36,50 @@ for ti = 1:nt
     T_vx(:,nx1)     = T_vx(:,nx);
     T_vz(1:nz1,Nx)  = T_vz(1:nz1,nx1);
 %     plume beneath lithosphere
-    midx = fix(L/2); 
-    
-    indexP = find(abs(xp(1,:)) <= midx+ (L*0.23) & abs(xp(1,:)) >= midx - (L*0.23));
-    T_mid(nz1,indexP) = T_bot+300;
-    indexvx = find(abs(xvx(1,:)) <= midx+ (L*0.23) & abs(xvx(1,:)) >= midx - (L*0.23));
-    T_vx(nz:Nz,indexvx) = T_bot+300;
-    indexvz = find(abs(xvz(1,:)) <= midx+ (L*0.23) & abs(xvz(1,:)) >= midx - (L*0.23));
-    T_vz(nz:Nz,indexvz) = T_bot+300;
+    midx = fix(L/2);    
+    indexP = find(abs(xp(1,:)) <= midx+ (L*0.25) & abs(xp(1,:)) >= midx - (L*0.25));
+%     indexP = 1:Nx; indexvx = 1:Nx; indexvz = 1:Nx;
+    T_mid(end-1:end,indexP) = T_bot+300;
+    indexvx = find(abs(xvx(1,:)) <= midx+ (L*0.25) & abs(xvx(1,:)) >= midx - (L*0.25));
+    T_vx(end-1:end,indexvx) = T_bot+300;
+    indexvz = find(abs(xvz(1,:)) <= midx+ (L*0.25) & abs(xvz(1,:)) >= midx - (L*0.25));
+    T_vz(end-1:end,indexvz) = T_bot+300;
    
 
-    [Rho_vx,Rho_vz,Rho_mid] = Thermal_expansion(Rho0,...
+     [Rho_vx,Rho_vz,Rho_mid] = Thermal_expansion(Rho0,...
     T_mid,T_vx,T_vz,Alpha_vx,Alpha_vz,Alpha_mid,T_top);    
 
     % ignore nonlinearity for the time being
-        % % =============================================================
-        % % Solve momentum and continuity equations
-        % % =============================================================
-        Pscale  = Eta_mantle/(dx+dz)*2; %pressure scaling coefficient, minimum viscosity
-        [P_out,vx_out,vz_out,vx_mid,vz_mid] = stokes_continuity_noair(nx,nz,Nx,Nz,...
-        nx1,nz1,dx,dz,Pscale,...
-        Eta_out,Eta_mid,Rho_vx,Rho_vz,gx,gz,bctop,bcbottom,bcleft,bcright);
+    % % =============================================================
+    % % Solve momentum and continuity equations
+    % % =============================================================
+    Pscale  = Eta_mantle/(dx+dz)*2; %pressure scaling coefficient, minimum viscosity
+    [P_out,vx_out,vz_out,vx_mid,vz_mid] = stokes_continuity_noair(nx,nz,Nx,Nz,...
+    nx1,nz1,dx,dz,Pscale,...
+    Eta_out,Eta_mid,Rho_vx,Rho_vz,gx,gz,bctop,bcbottom,bcleft,bcright);
     
-        % Define timestep dt_m for marker displacement
-        dt1 = CFL * min( dx/2/max(abs(vx_out(:))) , dz/2/max(abs(vz_out(:))) );
-%         dt2 = (dx^2)/2/max(abs(Kappa_mantle*RhoCp_mantle));
-%         dt = min(dt1,dt2);
-%         dt      = dt*dtkoef;
-%         maxvx   = max(max(abs(vx_out)));
-%         maxvz   = max(max(abs(vz_out)));
-%         if(dt*maxvx>dxzmax*dx)
-%             dt  = dxzmax*dx/maxvx;
-%         end
-%         if(dt*maxvz>dxzmax*dz)
-%             dt  = dxzmax*dz/maxvz;
-%         end
-%         
-    dt2 = CFL *dx*dz/max(max(abs(Kappa_mantle./Rho_mid(2:nz1,2:nx1)./Cp_mid(2:nz1,2:nx1))));
+    % Define timestep dt_m for marker displacement
+    dt1 = CFL * min( dx/2/max(abs(vx_out(:))) , dz/2/max(abs(vz_out(:))) ); %maximum timestep for advection       
+    dt2 = CFL *dx/2*dz/2/max(max(abs(Kappa_mantle./Rho_mid(2:nz1,2:nx1)./Cp_mid(2:nz1,2:nx1)))); %maximum timestep for T diffusion
     dt = min(dt2,dt1);
 
-    % % =============================================================
-    % % Update eulerian temperature, correct dt for maximum temperature
-    % % =============================================================
-    switch Tsolver
-        case 'implicit'           
-            [Epsxz,Sigxz,Epsxx,Sigxx,Hs,Ha,T_diff,dT] =...
-            Update_Temperature(Epsxz,Sigxz,Epsxx,Sigxx,Hs,Ha,nx1,nz1,dx,dz,...
-            vx_out,vz_out,Eta_out,Eta_mid,Rho_vz,Alpha_mid,T_mid,gz,...
-            Nx,Nz,k_vx,k_vz,Rho_mid,Cp_mid,Hr,T_top,dt);
-        case 'explicit'
-            
-    end
-    
-    T_diff = full(T_diff);
-    
-%     Output = 'Fromm'
-    [dTdt,dMdt] = Advection_solver(vx_out,vz_out,vx_mid,vz_mid,T_diff,Material,dz,dx,nx,nz,nx1,nz1,'fromm');
-    [dTdt1,dMdt1] = Advection_solver(vx_out,vz_out,vx_mid,vz_mid,T_diff,Material,dz,dx,nx,nz,nx1,nz1,'first upwind');
-    [dTdt2,dMdt2] = Advection_solver(vx_out,vz_out,vx_mid,vz_mid,T_diff,Material,dz,dx,nx,nz,nx1,nz1,'second upwind');
-    [dTdt3,dMdt3] = Advection_solver(vx_out,vz_out,vx_mid,vz_mid,T_diff,Material,dz,dx,nx,nz,nx1,nz1,'third upwind');
-    dTdt = full(dTdt); dMdt = full(dMdt);
-    if ti == 1
-        T_mid(2:end-1,2:end-1)      = T_diff(2:end-1,2:end-1)   - dTdt.*dt;
-        Material(2:end-1,2:end-1)   = round(Material(2:end-1,2:end-1) - dMdt.*dt);
-    else
-        T_mid(2:end-1,2:end-1)      = T_diff(2:end-1,2:end-1)   - (dTdt+dTdt0)./2.*dt;
-        Material(2:end-1,2:end-1)   = round(Material(2:end-1,2:end-1) - (dMdt+dMdt0)./2.*dt);
-    end
-    dTdt0 = dTdt; dMdt0 = dMdt;
+    %update temperature by diffusion and advection
+    [Epsxz,Sigxz,Epsxx,Sigxx,Hs,Ha,T_out,lapl_T0,adv_T0,dMdt0,Material] =...
+    Update_Temperature(Epsxz,Sigxz,Epsxx,Sigxx,Hs,Ha,nx1,nz1,dx,dz,...
+    vx_out,vz_out,vx_mid,vz_mid,Eta_out,Eta_mid,Rho_vz,Alpha_mid,T_mid,gz,...
+    Nx,Nz,k_vx,k_vz,Rho_mid,Cp_mid,Hr,T_top,dt,Tsolver,Material,ti,lapl_T0,adv_T0,dMdt0);    
     
     time = dt+time; 
     
-    if ~mod(ti,5) || ti==1
-        
-%         subplot(2,1,1)
-%     pcolor(xp(2:nx1),zp(2:nz1),T_diff(2:nx1,2:nz1))
-%     colormap(subplot(2,1,1),flipud(cm))
-%     axis ij image;
-%     shading flat; 
-%     colorbar
-%     title('colormap of Temperature')
-%     hold on
-%     contour(xp(2:nx1),zp(2:nz1),T_diff(2:nx1,2:nz1),[T_top:(T_bot+300-T_top)/10:T_bot+300],'k')
-%     
-%     subplot(2,1,2)
-%     pcolor(xp(2:nx1),zp(2:nz1),dTdt*dt)
-%     colormap(subplot(2,1,2),jet)
-%     axis ij image;
-%     shading flat; 
-%     colorbar
-%     title('colormap of Temperature')
-%     hold on
-%     quiver(xp(3:5:Nx),zp(3:5:Nz),vx_mid(3:5:nz1,3:5:Nx),vz_mid(3:5:Nz,3:5:Nx),'k')
-    run('plotfigures2')
-    profile report
-    pause(0.1)
-    ti
+    if ~mod(ti,20) || ti==1
+        run('plot   figures2')
+%     profile report
+%         saveas(figure(1),['../out/', RunID, '/Ti', num2str(ti), '.jpg'])
+        pause(0.1)
+        ti
     end
     
-    
+    T_mid = T_out;
     
 end
 
